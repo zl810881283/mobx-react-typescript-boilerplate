14f821a7773c53d905138496125eb4ea2529a2bc
zl <810881283@qq.com>
2018-02-01 12:52:49 +0800

    add my modify

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,84 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
+import { observable, action, useStrict, runInAction } from 'mobx';
+import { observer, inject, Provider } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
+useStrict(true)
+
+function delay(ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
 class AppState {
     @observable timer = 0;
 
     constructor() {
         setInterval(() => {
-            this.timer += 1;
+            runInAction("add one",() => {
+                this.timer += 1;
+            })
         }, 1000);
     }
 
-    resetTimer() {
+    @action
+    resetTimer = () => {
         this.timer = 0;
+    }
+
+    @action
+    set100 = async () => {
+        await delay(3000)
+        runInAction("sync set 100",() => {
+            this.timer = 100
+        })
     }
 }
 
-@observer
-class TimerView extends React.Component<{appState: AppState}, {}> {
+@inject((stores: any) => {
+    const appState = stores.appState as AppState
+    return {
+        set100: appState.set100,
+        resetTimer: appState.resetTimer,
+        timer: appState.timer
+    }
+})
+class TimerView extends React.Component<any, {}> {
     render() {
         return (
-            <div>
-                <button onClick={this.onReset}>
-                    Seconds passed: {this.props.appState.timer}
+            <>
+            <button onClick={this.onReset}>
+                Seconds passed: {this.props.timer}
+            </button>
+            <button onClick={this.onSet100}>
+                set 100
                 </button>
-                <DevTools />
-            </div>
+            <DevTools />
+            </>
         );
-     }
+    }
 
-     onReset = () => {
-         this.props.appState.resetTimer();
-     }
+    onReset = () => {
+        this.props.resetTimer();
+    }
+
+    onSet100 = () => {
+        this.props.set100();
+    }
 };
 
 const appState = new AppState();
-ReactDOM.render(<TimerView appState={appState} />, document.getElementById('root'));
+
+class Root extends React.Component<any, {}>{
+    render() {
+        return (
+            <Provider appState={appState}>
+                <TimerView />
+            </Provider>
+        )
+    }
+}
+ReactDOM.render(<Root />, document.getElementById('root'));
